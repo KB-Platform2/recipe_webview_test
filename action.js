ea48d4730d35dfca451e7a44b77febea53c922e4
dkkeystore <113659137+dkkeystore@users.noreply.github.com>
2022-09-27 17:48:02 +0900

    add arrayBufferToBase64 function

--- action.js
+++ action.js
@@ -15,6 +15,16 @@ function openGallery(msg) {
 }
 
 function getSelectedImage(base64Image) {
-    let imgSrc = "data:image/png;base64," + btoa(String.fromCharCode.apply(null, new Uint8Array(base64Image)));
+    let imgSrc = "data:image/png;base64," + arrayBufferToBase64(base64Image);
     document.getElementById('selectedImage').src = imgSrc;
+}
+
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
 }

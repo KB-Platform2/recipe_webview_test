763ed442e7ac709f6c37da89cb6849458b38c1e4
Sangyong Lee <sy9902.lee@gmail.com>
2022-10-06 14:28:49 +0900

    test

--- action.js
+++ action.js
@@ -43,18 +43,18 @@ function openShare() {
 function openLogin() {
 	const rand = Math.floor(Math.random() * 10000000);
     try {
-		alert("로그인 되었습니다. 토큰값이 변경되었습니다." + rand);
+		alert("로그인 되었습니다. 토큰값이 변경되었습니다.." + rand);
 		//loginNativeCheck.postMessage(rand);
 		
 		window.addEventListener("flutterInAppWebViewPlatformReady", function(event) {
-                window.flutter_inappwebview.callHandler('loginNativeCheck')
+                window.flutter_inappwebview.callHandler('loginNativeCheck', 'Message From Server JS')
                   .then(function(result) {
                     console.log(JSON.stringify(result));
 					alert("Receive Response" + JSON.stringify(result));
                 });
             });
-		
-		
+
+
 		
     } catch (err) {
         alert(err.message);
